6a7c0fde0db3346b81821bf9f5f98cbd102460f4
Katsutoshi Horie <mps299792458@gmail.com>
2017-10-01 17:26:51 +0900

    Introduce speed adjustment

--- src/main.cpp
+++ src/main.cpp
@@ -197,14 +197,19 @@ int main() {
         map_waypoints_dy.push_back(d_y);
     }
 
-    h.onMessage([&map_waypoints_x,
+    int lane = 1;
+    double ref_vel = 0.0;
+
+    h.onMessage([&lane,
+                 &map_waypoints_x,
                  &map_waypoints_y,
                  &map_waypoints_s,
                  &map_waypoints_dx,
-                 &map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws,
-                                    char *data,
-                                    size_t length,
-                                    uWS::OpCode opCode) {
+                 &map_waypoints_dy,
+                 &ref_vel](uWS::WebSocket<uWS::SERVER> ws,
+                           char *data,
+                           size_t length,
+                           uWS::OpCode opCode) {
                     // "42" at the start of the message means there's
                     // a websocket message event.
                     // The 4 signifies a websocket message
@@ -241,6 +246,37 @@ int main() {
                                 // on the same side of the road.
                                 auto sensor_fusion = j[1]["sensor_fusion"];
 
+                                if (previous_path_x.size() > 0) { car_s = end_path_s; }
+
+                                bool too_close = false;
+
+                                for (int i = 0; i < sensor_fusion.size(); i++) {
+                                    double d = sensor_fusion[i][6];
+                                    if (d < (2 + 4 * lane + 2)
+                                        && d > (2 + 4 * lane - 2)) {
+                                        double vx = sensor_fusion[i][3];
+                                        double vy = sensor_fusion[i][4];
+                                        double check_speed = sqrt(vx * vx + vy * vy);
+                                        double check_car_s = sensor_fusion[i][5];
+
+                                        check_car_s += static_cast<double>
+                                                       (previous_path_x.size())
+                                                       * .02 * check_speed;
+
+                                        if (check_car_s > car_s
+                                            && (check_car_s - car_s) < 30) {
+                                            too_close = true;
+                                            break;
+                                        }
+                                    }
+                                }
+
+                                if (too_close) {
+                                    ref_vel -= .224;
+                                } else if (ref_vel < 49.5) {
+                                    ref_vel += .224;
+                                }
+
                                 vector<double> ptsx;
                                 vector<double> ptsy;
 
@@ -272,7 +308,6 @@ int main() {
                                     ptsy.push_back(ref_y);
                                 }
 
-                                const int lane = 1;
                                 auto next_wp0 = getXY(car_s+30,
                                                       2+4*lane,
                                                       map_waypoints_s,
@@ -326,7 +361,6 @@ int main() {
                                                           + target_y * target_y);
 
                                 double x_add_on = 0.;
-                                const double ref_vel = 40.0;
                                 for (int i = 1; i <= 50 - previous_path_x.size(); ++i) {
                                     double n = target_dist / (.02 * ref_vel / 2.24);
                                     double x_point = x_add_on + target_x / n;
